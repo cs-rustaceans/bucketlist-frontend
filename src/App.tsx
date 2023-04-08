faaa534c622f5dd10acebe514ac6edcbc4ef8eb2
Alex Pop <popalexnicolae@gmail.com>
2023-04-08 11:07:50 +0300

    Add LoginPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { HeadProvider } from "react-head";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages";
+import LoginPage from "./pages/login";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
   },
 ]);
 
