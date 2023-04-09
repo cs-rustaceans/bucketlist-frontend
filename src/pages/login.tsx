c0feb98cfd2c9bd0526f3274f32eadc565f31833
Laurcons <laurcons@outlook.com>
2023-04-10 01:00:43 +0300

    feat: simplified login form code

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,42 +4,14 @@ import { NavigateFunction, useNavigate } from "react-router-dom";
 import Button from "../components/Button";
 import Layout from "../components/Layout";
 import TextInput from "../components/TextInput";
-import { BACKEND_API_URL } from "../constants";
-
-const handleLogin = async (
-  event: React.FormEvent,
-  email: string,
-  password: string,
-  setError: React.Dispatch<React.SetStateAction<string>>,
-  navigate: NavigateFunction
-) => {
-  event.preventDefault();
-  const response = await fetch(`${BACKEND_API_URL}/login`, {
-    method: "POST",
-    body: JSON.stringify({ email, password }),
-    headers: { "Content-Type": "application/json" },
-  });
-  if (!response.ok) {
-    setError("Invalid email or password.");
-    return;
-  }
-  const role = await response.text();
-  Cookies.set("email", email);
-  Cookies.set("password", password);
-  Cookies.set("role", role); // TODO: get rid of this
-  if (role === "admin") {
-    navigate("/admin");
-  } else if (role === "employee") {
-    navigate("/employee");
-  }
-};
+import useAxios from "../lib/hooks/useAxios";
 
 const EmailInput = ({
   email,
-  setEmail,
+  onChange,
 }: {
   email: string;
-  setEmail: React.Dispatch<React.SetStateAction<string>>;
+  onChange: (email: string) => void;
 }) => {
   return (
     <>
@@ -51,7 +23,7 @@ const EmailInput = ({
         id="email"
         name="email"
         value={email}
-        onChange={event => setEmail(event.target.value)}
+        onChange={event => onChange(event.target.value)}
         required
       />
     </>
@@ -60,10 +32,10 @@ const EmailInput = ({
 
 const PasswordInput = ({
   password,
-  setPassword,
+  onChange,
 }: {
   password: string;
-  setPassword: React.Dispatch<React.SetStateAction<string>>;
+  onChange: (password: string) => void;
 }) => {
   return (
     <>
@@ -75,7 +47,7 @@ const PasswordInput = ({
         id="password"
         name="password"
         value={password}
-        onChange={event => setPassword(event.target.value)}
+        onChange={event => onChange(event.target.value)}
         required
       />
     </>
@@ -83,25 +55,40 @@ const PasswordInput = ({
 };
 
 const LoginForm = () => {
-  const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const axios = useAxios();
+
+  const handleLogin = async () => {
+    try {
+      const { data: role } = await axios.post(`/login`, {
+        email,
+        password,
+      });
+      Cookies.set("email", email);
+      Cookies.set("password", password);
+      Cookies.set("role", role); // TODO: get rid of this
+      if (role === "admin") {
+        navigate("/admin");
+      } else if (role === "employee") {
+        navigate("/employee");
+      }
+    } catch (err: any) {
+      setError(err.toString());
+    }
+  };
 
   return (
     <div className="flex justify-center items-center h-screen">
-      <form
-        className="max-w-md w-full p-6 rounded-lg shadow-lg"
-        onSubmit={event =>
-          handleLogin(event, email, password, setError, navigate)
-        }
-      >
+      <div className="max-w-md w-full p-6 rounded-lg shadow-lg">
         <h2 className="text-2xl font-semibold mb-6">Login</h2>
-        <EmailInput email={email} setEmail={setEmail} />
-        <PasswordInput password={password} setPassword={setPassword} />
+        <EmailInput email={email} onChange={setEmail} />
+        <PasswordInput password={password} onChange={setPassword} />
         {error && <p className="text-red-500 mb-2">{error}</p>}
-        <Button type="submit">Login</Button>
-      </form>
+        <Button onClick={handleLogin}>Login</Button>
+      </div>
     </div>
   );
 };
